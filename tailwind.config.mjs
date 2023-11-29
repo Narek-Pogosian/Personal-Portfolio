6b79d105423eea8cec22111e1a1ab8009cb3e1c1
Narek Pogosian <narekpogosian2@gmail.com>
2023-11-30 00:42:55 +0100

    edited layout, added cva, made the Button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,34 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			
+		},
+		extend: {
+			colors: {
+				primary: {
+          DEFAULT: "hsl(var(--primary))",
+          300: "hsl(var(--primary-300))",
+          400: "hsl(var(--primary-400))",
+          600: "hsl(var(--primary-600))",
+        },
+				background: {
+					DEFAULT: "hsl(var(--background))"
+				},
+				foreground: {
+					DEFAULT: "hsl(var(--foreground))"
+				}
+			},
+			animation: {
+				shine: "shine 1.5s infinite",
+			},
+			keyframes: {
+				shine: {
+					"100%": { left: "100%" },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
