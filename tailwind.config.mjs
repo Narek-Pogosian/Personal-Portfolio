610473b10eba6f2dd3548fcaa08c972d8848c899
Narek Pogosian <narekpogosian2@gmail.com>
2023-12-01 20:26:17 +0100

    added input, uppdated colors, added prettier config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		container: {
-			center: true,
-			
-		},
-		extend: {
-			colors: {
-				primary: {
-          DEFAULT: "hsl(var(--primary))",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary-500))",
           300: "hsl(var(--primary-300))",
           400: "hsl(var(--primary-400))",
           600: "hsl(var(--primary-600))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-				background: {
-					DEFAULT: "hsl(var(--background))"
-				},
-				foreground: {
-					DEFAULT: "hsl(var(--foreground))"
-				}
-			},
-			animation: {
-				shine: "shine 1.5s infinite",
-			},
-			keyframes: {
-				shine: {
-					"100%": { left: "100%" },
-				},
-			},
-		},
-	},
-	plugins: [],
-}
+        background: {
+          DEFAULT: "hsl(var(--background))",
+        },
+        foreground: {
+          DEFAULT: "hsl(var(--foreground))",
+        },
+      },
+      animation: {
+        shine: "shine 1.5s infinite",
+      },
+      keyframes: {
+        shine: {
+          "100%": { left: "100%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
