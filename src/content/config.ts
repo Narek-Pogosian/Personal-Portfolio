989b8d3a33ecfee3c6b5edcd62a533bec001f35a
Narek Pogosian <narekpogosian2@gmail.com>
2024-02-01 23:42:18 +0100

    reset hero section, fix header and svg background mix, swap icons in theme toggle

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,21 @@ const blogCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string().max(40, "Max 40 characters for title"),
-    tags: z.array(z.string()),
     description: z.string().max(80, "Max 80 characters for description"),
-    type: z.enum(["lesson", "guide"]),
+    tags: z.array(z.string()),
   }),
 });
 
-// const projectCollection = defineCollection({})
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string().max(40, "Max 40 characters for title"),
+    description: z.string().max(80, "Max 80 characters for description"),
+    techStack: z.array(z.string()),
+  }),
+});
 
 export const collections = {
   blog: blogCollection,
-  // projects: projectCollection
+  projects: projectCollection,
 };
